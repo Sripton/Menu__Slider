5df61c80ab30d7d975e0758fcb90c9fdfcee7b43
Elmar Erzikhanov <elmarerzikhanov@MacBook-Pro-Elmar.local>
2024-06-12 10:49:48 +0300

    Исправление ошибки в style.css .section__wrapper {height: 1000px;}

--- src/index.js
+++ src/index.js
@@ -4,25 +4,24 @@ window.addEventListener("DOMContentLoaded", () => {
     section__inner = document.querySelector(".section__inner"),
     lengthSection = section.length,
     heigthWrapper = window.getComputedStyle(section__wrapper).height;
-  let sliderIndex = 1;
+  let sliderIndex;
   let offsetSlide = 0;
   section__inner.style.height = `${100 * lengthSection}%`;
 
   section__inner.style.display = "flex";
   section__inner.style.flexDirection = "column";
   section__wrapper.style.overflow = "hidden";
-  section__inner.style.transition = "0.8s all";
+  section__inner.style.transition = "0.5s all";
   section__wrapper.style.position = "relative";
 
   const data__slide = document.querySelectorAll("[data-slide-to]");
   data__slide.forEach((elem, index) => {
-    elem.addEventListener("click", (e) => {
-      const eventTarget = e.target.getAttribute("data-slide-to");
+    elem.addEventListener("click", () => {
       sliderIndex = index;
       sliderIndex++;
       offsetSlide =
         +heigthWrapper.slice(0, heigthWrapper.length - 2) * (sliderIndex - 1);
-        section__inner.style.transform = `translateY(-${offsetSlide}px)`;
+      section__inner.style.transform = `translateY(-${offsetSlide}px)`;
     });
   });
 });
